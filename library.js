6e3f28f91e7f4e1b642cd60d5900fd6f9b37ff2a
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-06-08 11:02:45 +0200

    myLibrary array to stor all the books,function  addBookToLibrary calls newBook constructor and adds input in myLibrary array,function showLibrary for now is just to test everything was working as intended

--- library.js
+++ library.js
@@ -1,12 +1,32 @@
-function AddBook (title, author, pages, read) {
+function newBook (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        let info = "${this.title}" + " by " + "${this.author}" +
-        ", " + "${this.pages}" + ", " + "${this.read}";
-        return info;
+        const inf = this.title + " by " + this.author +
+        ", " + this.pages + " pages, " + this.read;
+        return inf;
+    };
+}
+
+const myLibrary = [];  //Array that will contain all books
+
+function addBookToLibrary () { //in sviluppo
+    const title = window.prompt("Insert book title:");
+    const author = window.prompt("Insert book author:");
+    const pages = window.prompt("Insert how many pages is long:");
+    const read = window.prompt("Did you read it? Yes or No");
+    const book = new newBook (title, author, pages, read);
+    myLibrary.push (book);
+}
+
+function showLibrary () {
+    const counter = myLibrary.length;
+    for (i=0; i<counter; i++){
+        console.log(myLibrary[i].info());
     }
 }
 
+document.getElementById("addBook").onclick = addBookToLibrary;
+document.getElementById("showLibrary").onclick = showLibrary;
